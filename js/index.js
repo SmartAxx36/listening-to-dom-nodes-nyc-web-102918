3e6b6e541bd3e3a63b71ff91e79f926df16ced3a
gavinmoore36 <gavizoid16@gmail.com>
2019-01-08 10:36:35 -0500

    Done

--- js/index.js
+++ js/index.js
@@ -1,2 +1,22 @@
 // using this file is optional
 // you can also load the code snippets in using your browser's console
+const main = document.querySelector('#main')
+const divFive = document.querySelector('#five')
+const inputField = document.querySelector('#input-field')
+
+main.addEventListener('click', (e) =>{
+  console.log(e.target.innerText)
+})
+divFive.addEventListener('click', (e) =>{
+  console.log(e.target.innerText)
+})
+inputField.addEventListener('keypress', function (e) {
+    var key = e.which || e.keyCode;
+    if (key === 13) {
+      const newText = e.target.value
+      divFive.innerHTML += `
+      <p>${newText}</p>
+      `
+    }
+})
+//simple but I finally figured out the enter key for eventlisteners
